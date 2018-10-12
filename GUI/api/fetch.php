c14963b2e80ca0a9c9999d8533277ab7865a35a9
Parth Shah <parthshahk@gmail.com>
2018-10-12 21:12:41 +0530

    Added GUI Framework

---
+++ GUI/api/fetch.php
@@ -0,0 +1,12 @@
+<?php
+    $con = mysqli_connect("localhost","parth","parth","moviedb");
+
+    $sql = 'select * from movies';
+
+    $result = mysqli_query($con, $sql);
+    $rows = array();
+    while($r = mysqli_fetch_assoc($result)) {
+        $rows[] = $r;
+    }
+    print json_encode($rows);
+?>
